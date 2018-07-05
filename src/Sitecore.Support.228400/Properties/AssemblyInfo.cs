26fdf69c7b65b6f9747006423e12945510fd9250
DK\psk <psk@sitecore.net>
2018-07-05 18:05:36 +0300

    Preparation

---
+++ src/Sitecore.Support.228400/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.228400")]
+[assembly: AssemblyProduct("Sitecore.Support.228400")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyDescription("Select Media Dialog executes two identical search requests. It was fixed so that only one search request is made.")]
+
